b6c56ef454986681f19ed399a2929092e7246719
fedemagri23 <fmagri@itba.edu.ar>
2025-06-12 01:33:01 -0300

    commit final

--- compile.sh
+++ compile.sh
@@ -1,14 +1,14 @@
 if [ "$1" == "buddy" ]; then
-    MM="BUDDY"
+    MM="buddy"
 else
-    MM=""
+    MM="naive"
 fi
 
 docker run -d -v "${PWD}:/root" --privileged -ti --name TPECOMPILER agodio/itba-so-multi-platform:3.0
 docker start TPECOMPILER
 docker exec -it TPECOMPILER make clean -C/root/Toolchain
 docker exec -it TPECOMPILER make clean -C/root/
-docker exec -it TPECOMPILER make all -C /root/Toolchain MMTYPE="$MM"
-docker exec -it TPECOMPILER make all -C /root/ MMTYPE="$MM"
+docker exec -it TPECOMPILER make all -C /root/Toolchain MEM_IMPL="$MM"
+docker exec -it TPECOMPILER make all -C /root/ MEM_IMPL="$MM"
 docker stop TPECOMPILER 
-docker rm TPECOMPILER 
+docker rm TPECOMPILER
